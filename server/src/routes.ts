10577b6d8ea6cba9edf3729a1a378d37e6e65f35
AugustoMarcelo <mrclgst10@gmail.com>
2020-08-04 16:33:01 -0300

    :tada: server created

---
+++ server/src/routes.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsControllers';
+
+const routes = Router();
+const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
+
+routes.get('/classes', classesController.index);
+routes.post('/classes', classesController.create);
+
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
+
+export default routes;
