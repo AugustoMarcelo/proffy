da60d55a462afa3d117b7f7f2a90d4c5a71ead42
AugustoMarcelo <mrclgst10@gmail.com>
2020-08-09 14:17:59 -0300

    :fire: removed unused component

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
 
