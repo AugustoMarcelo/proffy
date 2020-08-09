24fcbe426512448df66e9070f712519629c38d60
AugustoMarcelo <mrclgst10@gmail.com>
2020-08-09 14:17:13 -0300

    :sparkles: implemented onboarding page

---
+++ mobile/src/pages/Onboarding/index.tsx
@@ -0,0 +1,90 @@
+import React, { useRef } from 'react';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Swipe from 'react-native-swiper';
+
+import slide1Img from '../../assets/images/slide1.png';
+import slide2Img from '../../assets/images/slide2.png';
+import nextIcon from '../../assets/images/icons/next.png';
+import slideStudyIcon from '../../assets/images/icons/slideIconStudy.png';
+import slideGiveClassesIcon from '../../assets/images/icons/slideIconGiveClasses.png';
+
+import styles from './styles';
+
+function Onboarding() {
+  const swipeRef = useRef<Swipe>(null);
+  const { navigate } = useNavigation();
+
+  function goToNextSlide() {
+    swipeRef.current?.scrollBy(1);
+  }
+
+  function handleNavigateToLoginPage() {
+    navigate('Landing');
+  }
+
+  return (
+    <Swipe
+      ref={swipeRef} 
+      loop={false}
+      dotStyle={{
+        borderRadius: 1,
+        width: 4,
+        height: 4,
+      }}
+      activeDotStyle={{
+        borderRadius: 1,
+        width: 4,
+        height: 4,
+      }}
+      activeDotColor="#8257E5"
+      paginationStyle={{
+        justifyContent: 'flex-start',
+        paddingLeft: 40,
+        paddingBottom: 39,
+      }}
+    >
+      <View style={styles.slide}>
+        <View style={[styles.header, { backgroundColor: '#8257E5' }]}>
+          <Image source={slide1Img} resizeMode="contain" />
+          <Image source={slideStudyIcon} style={styles.slideIcon} />
+        </View>
+
+        <View style={styles.content}>
+          <Text style={styles.slideNumber}>01.</Text>
+          <Text style={styles.slideDescription}>
+            Encontre vários professores para ensinar você
+          </Text>
+          <TouchableOpacity 
+            onPress={goToNextSlide} 
+            style={styles.buttonNextSlide}
+          >
+            <Image source={nextIcon} />
+          </TouchableOpacity>  
+        </View>
+      </View>
+
+      <View style={styles.slide}>
+        <View style={[styles.header, { backgroundColor: '#04D361' }]}>
+          <Image source={slide2Img} resizeMode="contain" />
+          <Image source={slideGiveClassesIcon} style={styles.slideIcon} />
+        </View>
+
+        <View style={styles.content}>
+          <Text style={styles.slideNumber}>02.</Text>
+          <Text style={styles.slideDescription}>
+            Ou dê aulas sobre o que você mais conhece
+          </Text>
+          <TouchableOpacity
+            onPress={handleNavigateToLoginPage}
+            style={styles.buttonNextSlide}
+          >
+            <Image source={nextIcon} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Swipe>
+  )
+}
+
+export default Onboarding;
---
+++ mobile/src/pages/Onboarding/styles.ts
@@ -0,0 +1,54 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {},
+
+  slide: {
+    flex: 1,
+    backgroundColor: '#E5E5E5',
+  },
+
+  header: {
+    height: 350,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'relative',
+  },
+
+  slideIcon: {
+    position: 'absolute',
+    transform: [
+      { translateY: 20 }
+    ]
+  },
+
+  content: {
+    paddingTop: 66,
+    paddingLeft: 40,
+    position: 'relative',
+  },
+
+  slideNumber: {
+    fontFamily: 'Archivo_500Medium',
+    fontSize: 40,
+    color: '#6A6180',
+    opacity: 0.16,
+  },
+
+  slideDescription: {
+    fontFamily: 'Poppins_500Medium',
+    color: '#6A6180',
+    fontSize: 24,
+    maxWidth: 210,
+    marginTop: 24,
+  },
+
+  buttonNextSlide: {
+    position: 'absolute',
+    right: 28,
+    bottom: -70,
+  },
+});
+
+export default styles;
