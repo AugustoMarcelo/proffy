cc9fb29ed9dc0dc6bfed5b93c2812d16e9ad8de5
AugustoMarcelo <mrclgst10@gmail.com>
2020-08-09 14:17:13 -0300

    :sparkles: implemented onboarding page

--- mobile/src/routes/AppStack.tsx
+++ mobile/src/routes/AppStack.tsx
@@ -5,6 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Landing from '../pages/Landing';
 import GiveClasses from '../pages/GiveClasses';
 import StudyTabs from './StudyTabs';
+import Onboarding from '../pages/Onboarding';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -12,6 +13,7 @@ function AppStack() {
   return (
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
+        <Screen name="Onboarding" component={Onboarding} />
         <Screen name="Landing" component={Landing} />
         <Screen name="GiveClasses" component={GiveClasses} />
         <Screen name="Study" component={StudyTabs} />
