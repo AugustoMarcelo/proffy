fcc8a786d1bbdf00440bdac92f418030e64845ed
AugustoMarcelo <mrclgst10@gmail.com>
2020-08-11 10:45:23 -0300

    :sparkles: added Login page

--- mobile/src/routes/AppStack.tsx
+++ mobile/src/routes/AppStack.tsx
@@ -6,6 +6,7 @@ import Landing from '../pages/Landing';
 import GiveClasses from '../pages/GiveClasses';
 import StudyTabs from './StudyTabs';
 import Onboarding from '../pages/Onboarding';
+import Login from '../pages/Login';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -14,6 +15,7 @@ function AppStack() {
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
         <Screen name="Onboarding" component={Onboarding} />
+        <Screen name="Login" component={Login} />
         <Screen name="Landing" component={Landing} />
         <Screen name="GiveClasses" component={GiveClasses} />
         <Screen name="Study" component={StudyTabs} />
