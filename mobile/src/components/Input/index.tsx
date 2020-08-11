b687d680613d46eab1b0b357243f73ba5166946f
AugustoMarcelo <mrclgst10@gmail.com>
2020-08-11 10:34:06 -0300

    :lipstick: created form field components

---
+++ mobile/src/components/Input/index.tsx
@@ -0,0 +1,74 @@
+import React, { useState, useRef, useEffect, useCallback } from 'react';
+import { View, TextInput, TextInputProps, Text, StyleProp, ViewStyle } from 'react-native';
+import { useField } from '@unform/core';
+
+import styles from './styles';
+
+interface InputProps extends TextInputProps {
+  name: string;
+  label?: string;
+  containerStyle?: StyleProp<ViewStyle>,
+}
+
+interface InputValueReference {
+  value: string;
+}
+
+const Input: React.FC<InputProps> = ({
+  name,
+  label,
+  containerStyle = {},
+  ...rest
+}) => {
+  const { fieldName, registerField, defaultValue = '', error } = useField(name);
+
+  const inputElementRef = useRef<any>(null);
+  const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  useEffect(() => {
+    registerField<string>({
+      name: fieldName,
+      ref: inputValueRef.current,
+      path: 'value',
+      setValue(ref: any, value) {
+        inputValueRef.current.value = value;
+        inputElementRef.current.setNativeProps({ text: value });
+      },
+      clearValue() {
+        inputValueRef.current.value = '';
+        inputElementRef.current.clear();
+      },
+    });
+  }, [fieldName, registerField]);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputValueRef.current.value);
+  }, []);
+
+  return (
+    <View style={[styles.container, containerStyle, isFocused || isFilled ? { paddingVertical: 8 } : { paddingVertical: 20 } ]}>
+      {label && <Text style={[styles.label, isFocused || isFilled ? styles.labelFloating : {}]}>{label}</Text>}
+      {isFocused && <View style={styles.borderOnFocus} />}
+      <TextInput
+        ref={inputElementRef}
+        defaultValue={defaultValue}
+        style={styles.input}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        onChangeText={value => inputValueRef.current.value = value}
+        {...rest}
+      />
+    </View>
+  );
+}
+
+export default Input;
---
+++ mobile/src/components/InputPassword/styles.ts
@@ -0,0 +1,51 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'relative',
+    height: 64,
+    backgroundColor: '#fafafc',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#e6e6f0',
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+    paddingHorizontal: 24,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  label: {
+    position: 'absolute',
+    fontFamily: 'Poppins_400Regular',
+    color: '#9C98A6',
+    fontSize: 14,
+    top: 20,
+    left: 24,
+  },
+
+  labelFloating: {
+    top: 10,
+    fontSize: 10,
+  },
+
+  borderOnFocus: {
+    position: 'absolute',
+    width: 2,
+    height: 40,
+    backgroundColor: '#8257E5',
+    left: -1,
+    top: 12,
+    borderRadius: 5,
+  },
+
+  input: {
+    fontFamily: 'Poppins_400Regular',
+    fontSize: 14,
+    color: '#6A6180',
+    flex: 1,
+  }
+});
+
+export default styles;
