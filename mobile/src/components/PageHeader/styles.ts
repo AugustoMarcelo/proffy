cef125fdbe7570fcc20bdad149b963ae351cd482
AugustoMarcelo <mrclgst10@gmail.com>
2020-08-11 10:47:00 -0300

    :lipstick: starting new header style

--- mobile/src/components/PageHeader/styles.ts
+++ mobile/src/components/PageHeader/styles.ts
@@ -1,16 +1,35 @@
-import React from 'react';
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    padding: 40,
+    padding: 32,
+    paddingTop: 101,
     backgroundColor: '#8257e5',
+    position: 'relative',
   },
 
   topBar: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
+    backgroundColor: '#774DD6',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 101,
+    paddingHorizontal: 32,
+    paddingTop: 40,
+    borderBottomWidth: 1,
+    borderBottomColor: '#6842C2',
+    borderStyle: 'solid'
+  },
+
+  pageModeText: {
+    fontFamily: 'Archivo_400Regular',
+    color: '#D4C2FF',
+    fontSize: 14,
+    lineHeight: 15,
   },
 
   header: {
