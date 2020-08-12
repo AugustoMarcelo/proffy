f6328e34ae41c68300ebd12f949cdbe9f82c3fba
AugustoMarcelo <mrclgst10@gmail.com>
2020-08-12 15:46:35 -0300

    :art: eslint fix

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,23 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { AppLoading } from 'expo';
-import { 
+import {
   Archivo_400Regular,
   Archivo_500Medium,
   Archivo_600SemiBold,
   Archivo_700Bold,
-  useFonts
-} from '@expo-google-fonts/archivo'
-import { 
-  Poppins_400Regular, 
+  useFonts,
+} from '@expo-google-fonts/archivo';
+import {
+  Poppins_400Regular,
   Poppins_500Medium,
-  Poppins_600SemiBold
-} from '@expo-google-fonts/poppins'
+  Poppins_600SemiBold,
+} from '@expo-google-fonts/poppins';
 
 import AppStack from './src/routes/AppStack';
 
-export default function App() {
-  let [fontsLoaded] = useFonts({
+const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
     Archivo_400Regular,
     Archivo_500Medium,
     Archivo_600SemiBold,
@@ -28,13 +28,14 @@ export default function App() {
   });
 
   if (!fontsLoaded) {
-    return <AppLoading />
-  } else {      
-    return (
-      <>
-        <AppStack />
-        <StatusBar style="light" />
-      </>
-    );
+    return <AppLoading />;
   }
-}
+  return (
+    <>
+      <AppStack />
+      <StatusBar style="light" />
+    </>
+  );
+};
+
+export default App;
