87ce6a9ab3d20f1a313390300161ae2bc1cfe6d8
AugustoMarcelo <mrclgst10@gmail.com>
2020-08-15 21:43:06 -0300

    :sparkles: created give classes register page success

---
+++ mobile/src/pages/GiveClassesRegisterSuccess/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+import backgroundSuccessImg from '../../assets/images/background-success.png';
+
+import {
+  Container,
+  Content,
+  Background,
+  BoxIcon,
+  SuccessText,
+  InfoText,
+  GoToLoginButton,
+  GoToLoginButtonText,
+} from './styles';
+
+const GiveClassesRegisterSuccess: React.FC = () => {
+  const { navigate } = useNavigation();
+
+  function handleGoToLogin() {
+    navigate('Login');
+  }
+
+  return (
+    <Container>
+      <StatusBar style="light" />
+      <Content>
+        <Background source={backgroundSuccessImg} resizeMode="contain" />
+        <BoxIcon>
+          <FontAwesome5 name="check" color="#04D361" size={24} />
+        </BoxIcon>
+        <SuccessText>Cadastro Salvo!</SuccessText>
+        <InfoText>
+          Tudo certo, seu cadastro está na nossa lista de professores. Agora é
+          só ficar de olgo no seu WhatsApp.
+        </InfoText>
+      </Content>
+      <GoToLoginButton onPress={handleGoToLogin}>
+        <GoToLoginButtonText>Fazer login</GoToLoginButtonText>
+      </GoToLoginButton>
+    </Container>
+  );
+};
+
+export default GiveClassesRegisterSuccess;
