6d0548a6e6719af48c9e5dcdb2fc60cc4db53b75
AugustoMarcelo <mrclgst10@gmail.com>
2020-08-17 16:57:14 -0300

    :lipstick: finalized give classes UI

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -1,37 +1,222 @@
-import React from 'react';
+import React, { useCallback, useState, useRef } from 'react';
+import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import { FormHandles } from '@unform/core';
 
-import giveClassesBgImage from '../../assets/images/give-classes-background.png';
+import PageHeader from '../../components/PageHeader';
+import Dropdown from '../../components/Dropdown';
 
+import Input from '../Profile/Input';
 import {
   Container,
-  Content,
   Title,
-  Description,
-  OkButton,
-  OkButtonText,
+  Subtitle,
+  ProfileResume,
+  Avatar,
+  UserNameContainer,
+  UserName,
+  UserSubject,
+  ProfileData,
+  Form,
+  Fieldset,
+  LegendGroup,
+  Legend,
+  InputBlock,
+  AddNewScheduleButton,
+  AddNewScheduleButtonText,
+  ScheduleItem,
+  Options,
+  Border,
+  RemoveScheduleButton,
+  RemoveScheduleButtonText,
+  Footer,
+  SaveButton,
+  SaveButtonText,
+  InfoContainer,
+  InfoText,
+  InfoTextHighlight,
 } from './styles';
 
 const GiveClasses: React.FC = () => {
-  const { goBack } = useNavigation();
+  const formRef = useRef<FormHandles>(null);
+  const { navigate } = useNavigation();
+  const [week_days] = useState([
+    { label: 'Domingo', value: '0' },
+    { label: 'Segunda', value: '1' },
+    { label: 'Terça', value: '2' },
+    { label: 'Quarta', value: '3' },
+    { label: 'Quinta', value: '4' },
+    { label: 'Sexta', value: '5' },
+    { label: 'Sábado', value: '6' },
+  ]);
 
-  function handleNavigateBack() {
-    goBack();
-  }
+  const [times] = useState(
+    Array.from({ length: 24 }).map((_, i) => {
+      return {
+        label: i === 1 ? `${i} hora` : `${i} horas`,
+        value: `${i}:`.padEnd(i > 9 ? 5 : 4, '0'),
+      };
+    }),
+  );
+
+  const [scheduleItems, setScheduleItems] = useState([
+    { week_day: '', from: '', to: '' },
+  ]);
+
+  const addNewScheduleItem = useCallback(() => {
+    setScheduleItems([...scheduleItems, { week_day: '', from: '', to: '' }]);
+  }, [scheduleItems]);
+
+  const setScheduleItemValue = useCallback(
+    (position: number, field: string, value: string) => {
+      const updatedScheduleItems = scheduleItems.map((scheduleItem, index) => {
+        if (index === position) {
+          return { ...scheduleItem, [field]: value };
+        }
+
+        return scheduleItem;
+      });
+
+      setScheduleItems(updatedScheduleItems);
+    },
+    [scheduleItems],
+  );
+
+  const removeScheduleItem = useCallback(
+    (position: number) => {
+      const updatedScheduleItems = scheduleItems.filter(
+        (item, index) => position !== index,
+      );
+
+      setScheduleItems(updatedScheduleItems);
+    },
+    [scheduleItems],
+  );
+
+  const handleSubmit = useCallback(() => {
+    navigate('GiveClassesRegisterSuccess');
+  }, [navigate]);
 
   return (
     <Container>
-      <Content resizeMode="contain" source={giveClassesBgImage}>
-        <Title>Quer ser um Proffy?</Title>
-        <Description>
-          Para começar, você precisa se cadastrar como professor na nossa
-          plataforma web.
-        </Description>
-      </Content>
-
-      <OkButton onPress={handleNavigateBack}>
-        <OkButtonText>Tudo bem</OkButtonText>
-      </OkButton>
+      <PageHeader pageTitle="Dar aulas">
+        <Title>Que incrível que você quer dar aulas.</Title>
+        <Subtitle>
+          O primeiro passo é preencher este formulário de inscrição
+        </Subtitle>
+      </PageHeader>
+      <ProfileData showsVerticalScrollIndicator={false}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <Fieldset>
+            <Legend>Seus dados</Legend>
+            <ProfileResume>
+              <Avatar
+                source={{ uri: 'https://github.com/augustomarcelo.png' }}
+              />
+              <UserNameContainer>
+                <UserName>Marcelo Augusto</UserName>
+                <UserSubject>Português</UserSubject>
+              </UserNameContainer>
+            </ProfileResume>
+            <Input name="whatsapp" label="Whatsapp" keyboardType="phone-pad" />
+            <Input
+              name="bio"
+              label="Bio"
+              multiline
+              numberOfLines={4}
+              textAlignVertical="top"
+              autoCompleteType="off"
+            />
+          </Fieldset>
+          <Fieldset>
+            <Legend>Sobre a aula</Legend>
+            <Input name="subject" label="Matéria" />
+            <Input name="cost" label="Custo da sua hora por aula" />
+          </Fieldset>
+          <Fieldset>
+            <LegendGroup>
+              <Legend
+                style={{
+                  borderBottomWidth: 0,
+                  paddingBottom: 0,
+                  marginBottom: 0,
+                }}
+              >
+                Horários disponíveis
+              </Legend>
+              <AddNewScheduleButton onPress={addNewScheduleItem}>
+                <AddNewScheduleButtonText>+ Novo</AddNewScheduleButtonText>
+              </AddNewScheduleButton>
+            </LegendGroup>
+            {scheduleItems.map((scheduleItem, index) => (
+              <ScheduleItem
+                key={`${scheduleItem.week_day}_${scheduleItem.to}_${scheduleItem.from}`}
+              >
+                <Dropdown
+                  placeholder="Selecione..."
+                  label="Dia da semana"
+                  items={week_days}
+                  defaultValue={scheduleItem.week_day}
+                  arrowStyle={{ marginRight: 12 }}
+                  onChangeItem={(item: { value: string }) =>
+                    setScheduleItemValue(index, 'week_day', item.value)
+                  }
+                />
+                <InputBlock>
+                  <Dropdown
+                    zIndex={4000}
+                    label="Das"
+                    placeholder="Selecione"
+                    defaultValue={scheduleItem.from}
+                    items={times}
+                    onChangeItem={(item: { value: string }) =>
+                      setScheduleItemValue(index, 'from', item.value)
+                    }
+                    dropDownMaxHeight={100}
+                    showArrow={false}
+                  />
+                  <Dropdown
+                    zIndex={3000}
+                    label="Até"
+                    placeholder="Selecione"
+                    defaultValue={scheduleItem.to}
+                    items={times}
+                    onChangeItem={(item: { value: string }) =>
+                      setScheduleItemValue(index, 'to', item.value)
+                    }
+                    dropDownMaxHeight={100}
+                    showArrow={false}
+                  />
+                </InputBlock>
+                <Options>
+                  <Border />
+                  <RemoveScheduleButton
+                    onPress={() => removeScheduleItem(index)}
+                  >
+                    <RemoveScheduleButtonText>
+                      Excluir horário
+                    </RemoveScheduleButtonText>
+                  </RemoveScheduleButton>
+                  <Border />
+                </Options>
+              </ScheduleItem>
+            ))}
+          </Fieldset>
+        </Form>
+        <Footer>
+          <SaveButton onPress={() => formRef.current?.submitForm()}>
+            <SaveButtonText>Salvar cadastro</SaveButtonText>
+          </SaveButton>
+          <InfoContainer>
+            <Feather name="alert-octagon" color="#8257E5" size={32} />
+            <InfoText>
+              <InfoTextHighlight>Importante!</InfoTextHighlight>
+              {'\n'}
+              Preencha todos os dados
+            </InfoText>
+          </InfoContainer>
+        </Footer>
+      </ProfileData>
     </Container>
   );
 };
--- mobile/src/pages/GiveClasses/styles.ts
+++ mobile/src/pages/GiveClasses/styles.ts
@@ -1,46 +1,184 @@
 import styled from 'styled-components/native';
 import { RectButton } from 'react-native-gesture-handler';
+import { Form as Unform } from '@unform/mobile';
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #8257e5;
-  justify-content: center;
-  padding: 40px;
-`;
-
-export const Content = styled.ImageBackground`
-  flex: 1;
-  justify-content: center;
+  background-color: #f0f0f7;
+  padding-bottom: 23px;
 `;
 
 export const Title = styled.Text`
   font-family: 'Archivo_700Bold';
+  font-size: 24px;
+  line-height: 29px;
   color: #fff;
-  font-size: 32px;
-  line-height: 37px;
-  max-width: 180px;
+  margin-top: -80px;
+  margin-bottom: 16px;
 `;
 
-export const Description = styled.Text`
-  margin-top: 24px;
-  color: #d4c2ff;
-  font-size: 16px;
-  line-height: 26px;
+export const Subtitle = styled.Text`
   font-family: 'Poppins_400Regular';
-  max-width: 240px;
+  font-size: 14px;
+  line-height: 24px;
+  color: #d4c2ff;
+  margin-bottom: 40px;
 `;
 
-export const OkButton = styled(RectButton)`
-  margin: 40px 0;
-  background-color: #04d361;
-  height: 58px;
+export const ProfileResume = styled.View`
+  flex-direction: row;
   align-items: center;
-  justify-content: center;
-  border-radius: 8px;
+  margin-bottom: 24px;
 `;
 
-export const OkButtonText = styled.Text`
-  color: #fff;
-  font-size: 16px;
+export const Avatar = styled.Image`
+  height: 64px;
+  width: 64px;
+  border-radius: 32px;
+`;
+
+export const UserNameContainer = styled.View`
+  margin-left: 15px;
+`;
+
+export const UserName = styled.Text`
   font-family: 'Archivo_700Bold';
+  font-size: 20px;
+  line-height: 25px;
+  color: #32264d;
+`;
+
+export const UserSubject = styled.Text`
+  font-family: 'Poppins_400Regular';
+  font-size: 12px;
+  line-height: 20px;
+  color: #6a6180;
+`;
+
+export const ProfileData = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    paddingTop: 24,
+  },
+})`
+  margin: -40px 16px 0;
+  background-color: #fff;
+  border-width: 1px;
+  border-color: #e6e6f0;
+  border-radius: 8px;
+  overflow: hidden;
+`;
+
+export const Form = styled(Unform)`
+  padding: 0 24px;
+`;
+
+export const Fieldset = styled.View`
+  margin-bottom: 24px;
+`;
+
+export const Legend = styled.Text`
+  font-family: 'Archivo_600SemiBold';
+  font-size: 20px;
+  line-height: 30px;
+  color: #32264d;
+  border-bottom-width: 1px;
+  border-color: #e6e6f0;
+  padding-bottom: 8px;
+  margin-bottom: 24px;
+`;
+
+export const LegendGroup = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom-width: 1px;
+  border-color: #e6e6f0;
+  padding-bottom: 8px;
+  margin-bottom: 24px;
+`;
+
+export const InputBlock = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const AddNewScheduleButton = styled.TouchableOpacity``;
+
+export const AddNewScheduleButtonText = styled.Text`
+  font-family: 'Archivo_600SemiBold';
+  font-size: 14px;
+  line-height: 26px;
+  color: #8257e5;
+`;
+
+export const ScheduleItem = styled.View`
+  margin-bottom: 24px;
+`;
+
+export const Options = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Border = styled.View`
+  height: 1px;
+  flex: 1;
+  background-color: #e6e6f0;
+`;
+
+export const RemoveScheduleButton = styled.TouchableOpacity`
+  margin: 0 24px;
+`;
+
+export const RemoveScheduleButtonText = styled.Text`
+  font-family: 'Archivo_600SemiBold';
+  font-size: 12px;
+  line-height: 20px;
+  color: #e33d3d;
+`;
+
+export const Footer = styled.View`
+  border-top-width: 1px;
+  border-top-color: #e6e6f0;
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+  background-color: #fafafc;
+  padding: 25px 25px 0;
+  z-index: -1;
+`;
+
+export const SaveButton = styled(RectButton)`
+  background-color: #04d361;
+  flex-direction: row;
+  height: 56px;
+  border-radius: 8px;
+  justify-content: center;
+  align-items: center;
+  align-self: stretch;
+`;
+
+export const SaveButtonText = styled.Text`
+  color: #fff;
+  font-family: 'Archivo_600SemiBold';
+  font-size: 16px;
+`;
+
+export const InfoContainer = styled.View`
+  flex-direction: row;
+  margin: 24px 0;
+  align-items: center;
+`;
+
+export const InfoText = styled.Text`
+  font-family: 'Poppins_400Regular';
+  font-size: 12px;
+  line-height: 20px;
+  color: #a0a0b3;
+  margin-left: 20px;
+`;
+
+export const InfoTextHighlight = styled.Text`
+  color: #8257e5;
 `;
