3076eeb256a57bb2c49dd90bdf7357bf9d20a144
AugustoMarcelo <mrclgst10@gmail.com>
2020-10-03 21:24:08 -0300

    :sparkles: created routes to login, register and register succes pages

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -4,11 +4,17 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import TeacherList from './pages/TeacherList';
 import TeacherForm from './pages/TeacherForm';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import RegisterSuccess from './pages/RegisterSuccess';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Route path="/" exact component={Landing} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+      <Route path="/register-success" component={RegisterSuccess} />
       <Route path="/study" component={TeacherList} />
       <Route path="/give-classes" component={TeacherForm} />
     </BrowserRouter>
